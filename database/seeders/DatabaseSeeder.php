96a5e2e562b6c6c6d39896743001c84275dc4674
riccikoo <reivalaura@gmail.com>
2025-02-08 12:12:17 +0700

    seed for divisions and departements

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $departement = [
+            ['id'=>'E-Kraf','name'=>'Ekonomi Kreatif'],
+            ['id'=>'MedKom','name'=>'Media dan Komunikasi'],
+            ['id'=>'SDM','name'=>'Sumber Daya Manusia'],
+            ['id'=>'KesMa','name'=>'Kesejahteraan Manusia'],
+            ['id'=>'RisTek','name'=>'Riset dan Teknologi']
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $division = [
+            ['id'=>'KWU','name'=>'Kewirausahaan','dep'=>'E-Kraf'],
+            ['id'=>'Sponsor','name'=>'Sponsorship','dep'=>'E-Kraf'],
+            ['id'=>'MedPub','name'=>'Media dan Publikasi','dep'=>'MedKom'],
+            ['id'=>'HubEks','name'=>'Hubungan Eksternal','dep'=>'MedKom'],
+            ['id'=>'MSDM','name'=>'Management Sumber Daya Manusia','dep'=>'SDM'],
+            ['id'=>'PSDM','name'=>'Pengembangan Sumber Daya Manusia','dep'=>'SDM'],
+            ['id'=>'MiBa','name'=>'Minat dan Bakat','dep'=>'KesMa'],
+            ['id'=>'RTH','name'=>'Rumah Tangga Himpunan','dep'=>'KesMa'],
+            ['id'=>'Akademik','name'=>'Akademik','dep'=>'RisTek'],
+            ['id'=>'PemTek','name'=>'Pengembangan Teknolgi','dep'=>'RisTek'],
+        ];
+
+        foreach($departement as $d){
+            DB::table('departements')->insert([
+                'id'=>$d['id'],
+                'name'=>$d['name'],
+                'created_at'=>now(),
+                'updated_at'=>now(),
+            ]);
+        }
+
+        foreach($division as $d){
+            DB::table('divisions')->insert([
+                'id'=>$d['id'],
+                'name'=>$d['name'],
+                'departement_code'=>$d['dep'],
+                'created_at'=>now(),
+                'updated_at'=>now(),
+            ]);
+        }
+
     }
 }
